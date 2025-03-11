3aaf6c38d1f243a5ede854a45ecb0e6193cff212
Anton-prog879 <toha.kryt34@gmail.com>
2025-03-11 22:03:59 +0300

    new files added

---
+++ src/main/java/com/example/bookshop2/Bookshop2Application.java
@@ -0,0 +1,13 @@
+package com.example.bookshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookshop2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bookshop2Application.class, args);
+    }
+
+}
